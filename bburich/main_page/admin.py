7566b17f007cf027ccd5d85fc162871457f58010
Bogdan Burich <bogdan.buritch@yandex.ru>
2022-08-08 08:48:05 +0300

    update profile section

--- bburich/main_page/admin.py
+++ bburich/main_page/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Company, Experience, Skill, Technology
+from .models import Company, Experience, Skill, Technology, Project
 
 
 admin.site.site_header = 'Bogdan Burich website'
@@ -7,3 +7,4 @@ admin.site.register(Company)
 admin.site.register(Experience)
 admin.site.register(Skill)
 admin.site.register(Technology)
+admin.site.register(Project)
--- bburich/main_page/views.py
+++ bburich/main_page/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Experience, Skill, Project, Technology
 
 
 def index(request):
+    skills = Skill.objects.all()
+    technologies = Technology.objects.all()
+    work_experience = Experience.objects.select_related('company').all()
+    projects = Project.objects.all()
     template = 'main_page/index.html'
-    return render(request, template)
+
+    context = {
+        "skills": skills,
+        "technologies": technologies,
+        "work_experience": work_experience,
+        "projets": projects,
+    }
+    return render(request, template, context)
